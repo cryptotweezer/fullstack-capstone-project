3a90e416af3835aac94b5bd22e2d9709006d4c0f
cryptotweezer <cryptotweezer@gmail.com>
2025-04-19 20:32:30 -0400

    ✅ Develop Back-end Integration for Login

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
@@ -11,26 +10,22 @@ const logger = pino();         // Create a Pino logger instance
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
+// REGISTER ENDPOINT
 router.post('/register', async (req, res) => {
     try {
-        // Task 1: Connect to giftsdb in MongoDB
         const db = await connectToDatabase();
-
-        // Task 2: Access the collection
         const collection = db.collection("users");
 
-        // Task 3: Check for existing email
         const existingEmail = await collection.findOne({ email: req.body.email });
         if (existingEmail) {
+            logger.error('Email already exists');
             return res.status(400).json({ error: "User with this email already exists" });
         }
 
-        // Hash password
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
 
-        // Task 4: Save user details in database
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -39,19 +34,53 @@ router.post('/register', async (req, res) => {
             createdAt: new Date(),
         });
 
-        // Task 5: Create JWT authentication
         const payload = {
             user: {
                 id: newUser.insertedId,
             },
         };
         const authtoken = jwt.sign(payload, JWT_SECRET);
-
         logger.info('User registered successfully');
         res.json({ authtoken, email });
     } catch (e) {
         logger.error("Error registering user", e);
         return res.status(500).send('Internal server error');
+    }
+});
+
+// LOGIN ENDPOINT
+router.post('/login', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+
+        const theUser = await collection.findOne({ email: req.body.email });
+
+        if (theUser) {
+            const result = await bcryptjs.compare(req.body.password, theUser.password);
+            if (!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong password' });
+            }
+
+            const payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+
+            logger.info('User logged in successfully');
+            return res.status(200).json({ authtoken, userName, userEmail });
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+        logger.error("Error logging in", e);
+        return res.status(500).json({ error: 'Internal server error', details: e.message });
     }
 });
 
